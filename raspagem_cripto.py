036a671407bb47f7eaf7ea2048129e9d9745e619
Vinicius Miguel <viniciusmiguelgit2@gmail.com>
2023-09-15 22:32:18 -0300

    kaopdkasd

--- raspagem_cripto.py
+++ raspagem_cripto.py
@@ -9,6 +9,7 @@ from email.mime.base import MIMEBase
 from email import encoders
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 
 
 class Cripto():
@@ -16,20 +17,69 @@ class Cripto():
         self.site_link = ('https://www.cryptocompare.com/')
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
-    
+        self.site_map = {
+            'mostra':{
+                'most': '/html/body/div[1]/div[4]/div/div[2]/div/div/homepage-tabs-section/div/div/div/div[1]/div/div[3]/a'
+            }
+        }
     def abre(self):
         self.driver.get(self.site_link)
         sleep(3)
-        self.driver.find_element(By.XPATH, '/html/body/div[1]/div[4]/div/div[2]/div/div/homepage-tabs-section/div/div/div/div[1]/div/div[3]/a').click() # clica para mostrar todas as moedas
+        self.lazy_upload()
+        sleep(3)
+        self.driver.find_element(By.XPATH,self.site_map['mostra']['most']).click() # clica para mostrar todas as moedas
         
     def main(self):
         self.abre()
         self.raspa_dados()
-        sleep(12783971269)
+        sleep(1279)
 
     def raspa_dados(self):
-        sleep(10)
+        sleep(5)
+        self.contador = 1
+        while True:
+            xpaths = {
+                'moedas':{
+                    'nome': f'/html/body/div[1]/div[4]/div/div/div/coins-list-v2/div/div/table/tbody/tr[{self.contador}]/td[3]/a/h3/span[1]',
+                    'preco': f'/html/body/div[1]/div[4]/div/div/div/coins-list-v2/div/div/table/tbody/tr[{self.contador}]/td[4]/div',
+                    'total_vol': f'/html/body/div[1]/div[4]/div/div/div/coins-list-v2/div/div/table/tbody/tr[{self.contador}]/td[6]/div',
+                    'toptier_vol': f'/html/body/div[1]/div[4]/div/div/div/coins-list-v2/div/div/table/tbody/tr[{self.contador}]/td[7]/div',
+                    'next': f'/html/body/div[1]/div[4]/div/div/div/coins-list-v2/div/div/div[2]/div[2]/a'
+                }   
+                    
+            }
+            try:
+                self.lazy_upload()
 
+                n = self.driver.find_element(By.XPATH, xpaths['moedas']['nome']).text
+                p = self.driver.find_element(By.XPATH, xpaths['moedas']['preco']).text
+                t1 = self.driver.find_element(By.XPATH, xpaths['moedas']['total_vol']).text
+                t2 = self.driver.find_element(By.XPATH, xpaths['moedas']['toptier_vol']).text
+                lis = [f'{n}',f'{p}',f'{t1}',f'{t2}']
+                print(lis[0],lis[1],lis[2],lis[3])
+
+
+                sleep(0.5)
+                print(self.contador)
+                self.contador += 1
+            
+            except NoSuchElementException:
+                try:
+                    botao_proximo = self.driver.find_element(By.XPATH, xpaths['moedas']['next'])
+                    if botao_proximo:
+                        botao_proximo.click()
+                        sleep(4)
+                        print('navegando para proxima pagina !!!')
+                        self.contador += 1
+                except:
+                    break
+
+            except Exception as e:
+                print(f'error {e}')
+
+
+    def lazy_upload(self):
+        self.driver.execute_script("window.scrollBy(0,800);")
 
 
 
