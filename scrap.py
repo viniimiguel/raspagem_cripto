0c4f0fe483faa1b3771cc51fbe3ac5464ed68070
Vinicius Miguel <viniciusmiguelgit2@gmail.com>
2023-09-22 22:39:36 -0300

    asopdkpaos

---
+++ scrap.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.common.by import By
+import openpyxl
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Key
