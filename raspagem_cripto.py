42cccd1d54170e76996861be4f83de70a831ec1c
Vinicius Miguel <viniciusmiguelgit2@gmail.com>
2023-09-23 11:27:37 -0300

    aspdksoad

--- raspagem_cripto.py
+++ raspagem_cripto.py
@@ -1,1 +1,21 @@
-print('o  coddigo esta senddo refeito...')
+class Cripto():
+    def __init__(self) -> None:
+        pass
+        
+    def main(self):
+        pass
+
+    def abre(self):
+        pass
+
+    def raspagem(self):
+        pass
+
+    def lazy_upload(self):
+        pass
+
+    def planilha(self):
+        pass
+
+    def envia(self):
+        pass
