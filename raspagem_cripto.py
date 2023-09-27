d96bc892dcc38a02b639c8f7fb30c17db8eef3ba
Vinicius Miguel <viniciusmiguelgit2@gmail.com>
2023-09-26 23:09:15 -0300

    aoskdo

--- raspagem_cripto.py
+++ raspagem_cripto.py
@@ -1,6 +1,20 @@
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.common.by import By
+import openpyxl
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+from selenium.common.exceptions import NoSuchElementException
+
+
+
 class Cripto():
     def __init__(self) -> None:
-        pass
+        self.driver = webdriver.Chrome
+        self.site_link('https://www.kraken.com/prices?page=1')
         
     def main(self):
         pass
