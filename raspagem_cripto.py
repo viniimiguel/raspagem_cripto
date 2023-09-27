a229412f6a575f7447ccfb059a8a6300423857b7
Vinicius Miguel <viniciusmiguelgit2@gmail.com>
2023-09-26 23:22:02 -0300

    akopasd

--- raspagem_cripto.py
+++ raspagem_cripto.py
@@ -13,17 +13,41 @@ from selenium.common.exceptions import NoSuchElementException
 
 class Cripto():
     def __init__(self) -> None:
-        self.driver = webdriver.Chrome
-        self.site_link('https://www.kraken.com/prices?page=1')
-        
-    def main(self):
-        pass
+        self.driver = webdriver.Chrome()
+        self.site_link = ('https://www.kraken.com/prices?page=1')
 
     def abre(self):
-        pass
+        self.driver.get(self.site_link)
+        self.driver.maximize_window()
+        
+    def main(self):
+        self.abre()
+        self.raspagem()
+        sleep(123123213)
 
     def raspagem(self):
-        pass
+        self.site_map = {
+            'xp':{
+                'nome': '/html/body/div[1]/div/div/div/article/div/div[7]/div/div/div/div/div/div[3]/table/tbody/tr[1]/td[2]/div/a/div/span[1]',
+                'preco': '/html/body/div[1]/div/div/div/article/div/div[7]/div/div/div/div/div/div[3]/table/tbody/tr[1]/td[3]/span',
+                'change': '/html/body/div[1]/div/div/div/article/div/div[7]/div/div/div/div/div/div[3]/table/tbody/tr[1]/td[5]/span',
+            }
+        }
+        while True:
+            try:
+                nome = self.driver.find_element(By.XPATH,self.site_map['xp']['nome']).text
+                preco = self.driver.find_element(By.XPATH,self.site_map['xp']['preco']).text
+                change = self.driver.find_element(By.XPATH,self.site_map['xp']['change']).text
+
+                print(nome)
+                print(preco)
+                print(change)
+                
+            except NoSuchElementException:
+                break
+
+            except:
+                pass
 
     def lazy_upload(self):
         pass
@@ -33,3 +57,7 @@ class Cripto():
 
     def envia(self):
         pass
+
+
+cripto = Cripto()
+cripto.main()
